45d02c72f46c4938545b3d18ec36fcae73198bcf
Meuss <thomas.miller147@gmail.com>
2017-01-22 11:20:17 +0100

    leaderboard

--- leaderboard_print.js
+++ leaderboard_print.js
@@ -5,9 +5,10 @@ $(() => {
   $.getJSON('leaderboard.json', (data) => {
     data.sort((a, b) => a.finaltimeformatted.localeCompare(b.finaltimeformatted));
     let scoreboard = '';
-
+    let i = 1;
     $.each(data, (index, val) => {
-      scoreboard += `<div class="single-score"><span>${val.nickname}</span><span>${val.finaltime}</span></div>`;
+      scoreboard += `<div class="single-score"><span>${i}: ${val.nickname}</span><span>${val.finaltime}</span></div>`;
+      i++
     });
 
     $(scoreboard).appendTo('#leaderboard');
